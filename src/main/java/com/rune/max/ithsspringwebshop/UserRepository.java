86571da80f02d63503008d531bd659f750a1e892
Max Rune <max.rune@gmail.com>
2020-11-30 19:38:26 +0100

    add user class and its repository

---
+++ src/main/java/com/rune/max/ithsspringwebshop/UserRepository.java
@@ -0,0 +1,8 @@
+package com.rune.max.ithsspringwebshop;
+
+import org.springframework.data.repository.CrudRepository;
+import com.rune.max.ithsspringwebshop.User;
+
+public interface UserRepository extends CrudRepository<User, Integer> {
+    
+}
