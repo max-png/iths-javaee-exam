5bb5115dfee9c1b0846eb00a26ba8df00a2a5c06
Max Rune <max.rune@gmail.com>
2020-11-30 18:37:02 +0100

    init commit

---
+++ src/main/java/com/rune/max/ithsspringwebshop/IthsSpringWebshopApplication.java
@@ -0,0 +1,13 @@
+package com.rune.max.ithsspringwebshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IthsSpringWebshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IthsSpringWebshopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rune/max/ithsspringwebshop/IthsSpringWebshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rune.max.ithsspringwebshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IthsSpringWebshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
